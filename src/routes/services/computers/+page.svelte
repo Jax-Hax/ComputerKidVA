131b094002d4f2905e06e84f0b563690bf7feaa6
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-02-13 14:52:06 -0500

    commit before school ends

--- src/routes/services/computers/+page.svelte
+++ src/routes/services/computers/+page.svelte
@@ -1,20 +1,19 @@
 One of the products I offer is help picking out the perfect laptop for you. Whether you need something high end for the highest quality gaming, or just a simple low-power computer to check your email and look at the internet, I will find the perfect fit for you.
 The chart below is an example of what I could provide, with one laptop for each major catagory. If you need something specific, or don't understand how to pick out the perfect one, ask me and I will help you come up with a laptop for only $5.
-<div id="computerGrid">
+<section id="computerGrid">
 <div class="computerItem">
 <img src="/checkmark(1).jpg" alt="Girl in a jacket">
+<h1>Title</h1>
+<p>short description</p>
 </div>
-
-<div class="computerItem">
-<img src="/checkmark(1).jpg" alt="Girl in a jacket">
-</div>
-<div class="computerItem">
-<img src="/checkmark(1).jpg" alt="Girl in a jacket">
-</div>
-</div>
+</section>
 <style>
     #computerGrid{
         display: grid;
         grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+    }
+    .computerItem{
+        background: #ececec;
+        border-radius: 30px;
     }
 </style>
