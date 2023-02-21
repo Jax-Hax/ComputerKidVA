57e26941b24d1d93879665a9fd6d8954b4175a86
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-02-21 16:39:36 -0500

    adding projects and realized i need to build other

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,7 +13,7 @@
       projects = compRepair;
     }
     else{
-      projects = gameDev;
+      projects = server;
     }
 	}
 	/**
@@ -22,10 +22,10 @@
 	
   let compRepair = [
 		{
-			name: 'This website!',
+			name: 'Successfully built and upgraded multiple desktops.',
 			image: '/checkmark(1).jpg',
-			time: '3 months',
-			dateFinished: 2023,
+			time: '2 days each',
+			dateFinished: 2020-22,
 			link: 'https://computerkidva.com',
 			description: 'A website for my small business.'
 		}
@@ -50,7 +50,7 @@
 			description: 'A website for my small business.'
 		}
 	];
-  let gameDev = [
+  let server = [
 		{
 			name: 'c!',
 			image: '/checkmark(1).jpg',
@@ -62,29 +62,15 @@
 	];
   let projects = techSupport;
 </script>
-
-<h1>Background</h1>
-<p>
-	I have been coding for 6 years, and am currently a freshmen in high school. I have experience in
-	many fields, since I have been trying to learn every major field of computer science before I
-	choose my major in college. Those fields include: Game Development, Web Development, Server
-	Management, Cybersecurity, IT, and Hardware Building/Repair. I am currently working on expanding
-	my knowledge on Cybersecurity, and hoping to learn Machine Learning, as well as Electrical Engineering. My background allows me to be
-	the perfect fit for most all of your projects, and at a low cost as well!
-</p>
-<h1>Inspiration</h1>
-<p>
-	I started out wanting to be a coder just because I love to code, and eventually the salary also became
-	another motivation. A couple of years ago, however, I started doing a lot of research into climate
-	change (some of which is in a project below), and now I want to help contribute to stopping it.
-	My main plan right now is to became a Machine Learning engineer, so that I can help make some sort
-	of AI to help!
-</p>
+<section id="aboutMeHeader">
+<h1>Jax Bulbrook</h1>
+<p>I am a Systems Engineer, doing both hardware (tech support, product consulting, server management, and computer building) and software (website and game development)</p>
+</section>
 <h1>Projects</h1>
 <button on:click={() => setProject(0)}>Tech Support</button>
-<button on:click={() => setProject(1)}>Web Design</button>
+<button on:click={() => setProject(1)}>Web/Game Design</button>
 <button on:click={() => setProject(2)}>Computer Repair</button>
-<button on:click={() => setProject(3)}>Game Design</button>
+<button on:click={() => setProject(3)}>Server Management</button>
 <section id="Grid">
 	{#each projects as project}
 		<a href={project.link} target="”_blank”"
@@ -98,6 +84,15 @@
 </section>
 
 <style>
+  #aboutMeHeader{
+    padding: 8em 3em;
+  }
+  p{
+    font-size: 2rem;
+  }
+  h1{
+    font-size: 3rem;
+  }
 	#Grid {
 		display: grid;
 		gap: 1rem;
